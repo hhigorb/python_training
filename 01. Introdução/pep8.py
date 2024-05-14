0c8df52a35772902702ed46d72539130a6539095
Higor Henrique <hhigorb@outlook.com>
2024-05-14 20:11:38 -0300

    Create pep8.py

---
+++ 01. Introdução/pep8.py
@@ -0,0 +1,34 @@
+"""
+É possível ler sobre a PEP8 diretamente na documentação do Python: https://peps.python.org/pep-0008/
+
+A PEP 8 é um guia de estilo para escrita de código em Python, desenvolvido pela comunidade para promover consistência e
+legibilidade no código. Aqui está um resumo das principais diretrizes:
+
+1. Indentação: Use 4 espaços para cada nível de indentação. Evite usar tabs, a menos que seja uma convenção já estabelecida.
+
+2. Comprimento da linha: Limite as linhas a 79 caracteres para facilitar a leitura em telas menores. Para comentários ou
+docstrings, pode-se usar até 72 caracteres.
+
+3. Imports: Coloque todos os imports em seções separadas, com imports do Python padrão primeiro, seguidos por bibliotecas
+de terceiros e, por fim, imports locais. Evite importar tudo de um módulo (from module import *).
+
+4. Espaços em branco: Use espaços em branco de forma consistente. Não coloque espaços em branco imediatamente dentro de
+parênteses, colchetes ou chaves, e use uma linha em branco entre definições de funções e classes.
+
+5. Nomes: Use nomes descritivos para variáveis, funções e classes. Prefira minúsculas para nomes de variáveis, separando
+palavras com sublinhados (snake_case), e use CamelCase para nomes de classes.
+
+6. Comentários: Escreva comentários claros e concisos para explicar partes complicadas do código ou a intenção por trás
+de uma determinada implementação. Evite comentários óbvios ou redundantes.
+
+7. Docstrings: Inclua docstrings explicativas para módulos, funções, métodos e classes, seguindo o formato do Docstring
+do Python.
+
+8. Expressões e declarações: Prefira a clareza sobre a concisão. Evite expressões complexas em uma linha e não
+abrevie nomes desnecessariamente.
+
+9. Tratamento de exceções: Não use capturas de exceção em branco (except:), especifique explicitamente as
+exceções que você espera manipular.
+
+Seguir as diretrizes da PEP 8 pode ajudar a tornar o seu código Python mais legível e colaborativo.
+"""

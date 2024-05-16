c3d0815a1191e44d8d828fc662c6ef7206c36711
Higor Henrique <hhigorb@outlook.com>
2024-05-15 21:11:00 -0300

    Create comentarios.py

---
+++ 01. Introdução/comentarios.py
@@ -0,0 +1,35 @@
+"""
+Em Python, os comentários são usados para incluir informações explicativas no código-fonte que não são
+executadas pelo interpretador Python. Eles são úteis para documentar o código, tornando-o mais legível e
+compreensível para outros programadores (ou mesmo para você mesmo no futuro). Existem dois tipos principais
+de comentários em Python:
+
+1. Comentários de linha única: São criados precedendo o texto com o símbolo #. Todo o texto após #
+na mesma linha é considerado um comentário e não será executado pelo interpretador Python.
+"""
+
+# Este é um comentário de linha única em Python
+x = 5  # Esta linha declara uma variável chamada x e atribui o valor 5 a ela
+
+"""
+2. Comentários de várias linhas ou comentários em bloco: São usados para comentários que abrangem várias linhas.
+Eles são delimitados por três aspas simples ou três aspas duplas no início e no final do comentário.
+"""
+
+'''
+Este é um comentário
+de várias linhas em Python.
+Ele pode abranger múltiplas linhas
+sem a necessidade de usar # em cada linha.
+'''
+
+"""
+Este é outro exemplo de um comentário
+de várias linhas em Python.
+"""
+
+"""
+É importante usar comentários de forma adequada e significativa para garantir que o código seja facilmente
+compreendido por outros programadores e por você mesmo no futuro. Comentários excessivos ou desnecessários
+podem dificultar a leitura do código.
+"""

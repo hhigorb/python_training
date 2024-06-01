c244a7cd8ee59ea117e7bfe9a5b0ebdac78189d8
Higor Henrique <hhigorb@outlook.com>
2024-06-01 18:58:59 -0300

    Create variaveis.py

---
+++ 02. Variáveis e tipo de dados/variaveis.py
@@ -0,0 +1,14 @@
+"""
+Variáveis
+
+Variáveis em Python são nomes que são armazenados em memória que armazenam valores ou dados. Quando você cria uma variável e atribui
+um valor a ela, o Python reserva um espaço na memória para guardar esse valor, permitindo que você acesse e manipule esses dados facilmente pelo nome da variável.
+"""
+
+nome = "Maria"
+idade = 30
+altura = 1.75
+
+"""
+Aqui, as variáveis nome, idade e altura armazenam uma string, um inteiro e um ponto flutuante, respectivamente, em diferentes espaços na memória.
+"""

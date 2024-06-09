c71b19e5f5d841432be87193be3b523519cff61f
Higor Henrique <hhigorb@outlook.com>
2024-06-09 18:55:34 -0300

    Create pip.py

---
+++ 01. Introdução/pip.py
@@ -0,0 +1,40 @@
+"""
+Pip
+
+O pip é a ferramenta oficial para instalação e gerenciamento de pacotes no Python. Ele facilita a instalação de bibliotecas,
+a atualização, a remoção e a resolução de dependências, tornando o desenvolvimento em Python mais simples e eficiente.
+
+O pip tem várias utilidades, como:
+
+Instalar Pacotes: Instala bibliotecas do Python Package Index (PyPI).
+
+pip install nome-do-pacote
+
+Atualizar Pacotes: Mantém as bibliotecas atualizadas.
+
+pip install --upgrade nome-do-pacote
+
+Remover Pacotes: Remove bibliotecas que não são mais necessárias.
+
+pip uninstall nome-do-pacote
+
+Listar Pacotes: Mostra todas as bibliotecas instaladas.
+
+pip list
+
+Exportar/Importar Dependências: Gera um arquivo requirements.txt para compartilhar ou replicar o ambiente.
+
+pip freeze > requirements.txt
+pip install -r requirements.txt
+
+Em resumo, o pip é uma ferramenta essencial para gerenciar pacotes no Python, garantindo que suas bibliotecas estejam sempre atualizadas e bem organizadas.
+
+PyPI
+
+O PyPI, ou Python Package Index, é um repositório online onde os desenvolvedores Python compartilham e disponibilizam suas
+bibliotecas e pacotes Python para que outros desenvolvedores possam facilmente encontrar, baixar e utilizar em seus
+próprios projetos. É essencialmente uma biblioteca centralizada de pacotes Python que promove a colaboração e a
+reutilização de código dentro da comunidade Python.
+
+https://pypi.org/
+"""
